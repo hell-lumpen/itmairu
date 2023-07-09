b5e381da86890951f315e98ec0dab5147a3bc59e
Grubenko Maxim <mdgrubenko@maieducation>
2023-07-09 18:24:55 +0300

    add burger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 import logo from '../../assets/logo.png';
-
+import menuIcon from '../../assets/menuw.png';
 
 function Header() {
-    return (
-        <header className="header">
-            <div className='header_container'>
-                <div className="logo">
-                    <img src={logo} alt="Логотип" />
-                </div>
-                <nav className="nav">
-                    <ul>
-                    <li>Главная</li>
-                    <li>Бакалавриат</li>
-                    <li>Магистратура</li>
-                    <li>Мероприятия</li>
-                    <li>Приемная комиссия</li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
-}  
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="header_container">
+        <div className="logo">
+          <img src={logo} alt="Логотип" />
+        </div>
+        <div className="menu-icon" onClick={handleMenuClick}>
+          <img src={menuIcon} alt="Меню" />
+        </div>
+        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li>Главная</li>
+            <li>Бакалавриат</li>
+            <li>Магистратура</li>
+            <li>Мероприятия</li>
+            <li>Приемная комиссия</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
